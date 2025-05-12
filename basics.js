b06dd26032961998abffee5845862f59f8ee5dac
Anurag <anuragsharma921998@gmail.com>
2025-05-12 07:08:46 +0530

    Basic Syntax & Variables Task in JS

---
+++ basics.js
@@ -0,0 +1,100 @@
+//"Print 'Hello, World!' to the console.",
+//Sol.1
+console.log("Hello, World!");
+
+//"Declare a variable and assign a number to it.",
+//Sol.2
+let a = 5;
+console.log(a);
+
+//"Declare a variable and assign a string to it.",
+//Sol.3
+let str = "anurag";
+console.log(str);
+
+//"Declare a constant and assign a boolean value.",
+//Sol.4
+const isRaining = true;
+console.log("isRainy ? :", isRaining);
+
+//"Add two numbers and print the result.",
+//Sol.5
+let num1 = 10;
+let num2 = 20;
+console.log("Sum of two numbers: ",num1 + num2);
+
+//"Subtract two numbers and print the result.",
+//Sol.6
+let sub1 = 20;
+let sub2 = 10;
+let value = sub1 - sub2;
+console.log("Subtraction of two numbers is : ",value);
+
+//"Multiply two numbers and print the result.",
+//Sol.7
+let mul1 = 8;
+let mul2 = 10;
+console.log("Multiplication of two numbers :", mul1 * mul2);
+
+//"Divide two numbers and print the result.",
+//Sol.8
+let divi1 = 20;
+let divi2 = 4;
+let finalValue = divi1 / divi2;
+console.log("Division of two values: ", finalValue);
+
+//"Find the remainder of a division.",
+//Sol.9
+let mod1 = 20;
+let mod2 = 3;
+console.log("Module of division :", mod1 % mod2);
+
+//"Use `typeof` to check the type of a variable.",
+//Sol.10
+let myStr = "My Name is anurag";
+let age = 20;
+console.log(typeof myStr);
+console.log(typeof age);
+
+//"Swap two variables' values.",
+//Sol.11
+let value1 = 90;
+let value2 = 10;
+[value1 , value2] = [value2 , value1];
+console.log(value1);
+console.log(value2);
+
+//"Use template literals to combine strings and variables.",
+//Sol.12
+let nameIs = "anurag";
+let myAge = 20;
+console.log(`My Name is ${nameIs} and I'm ${age} year old.`);
+
+//"Concatenate two strings using `+`.",
+//Sol.13
+let firstName = "Anurag";
+let lastname = "Sharma";
+let fullName = firstName +" " + lastname;
+console.log(fullName);
+
+//"Create a variable without initializing it.",
+//Sol.14
+let a1;
+a1 = 2004;
+console.log(a1);
+console.log( typeof a1);
+
+//"Use `let` vs `var` in a code example.",
+//Sol.15
+let apnaGao = "Barwala";
+apnaGao = "Haryana";
+console.log(apnaGao);
+
+// let apnaGao = "error" 
+
+var teraSher = "Delhi";
+teraSher = "pune";
+console.log(teraSher);
+
+var teraSher = "Noida";
+console.log(teraSher);
