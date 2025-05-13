d51bed96ec078b6e3fdb19da8244a10d4c1510de
Anurag <anuragsharma921998@gmail.com>
2025-05-13 21:32:17 +0530

    JS-string-Task

---
+++ string.js
@@ -0,0 +1,23 @@
+//Create a string and print its length.
+//Sol.1
+let str = "anurag";
+console.log(str);
+console.log(str.length);
+
+//Convert a string to uppercase.
+//Sol.2
+let myName = "anurag sharma";
+let newName = myName.toUpperCase();
+console.log(newName);
+
+//Convert a string to lowercase.
+//Sol.3
+let lower = "LOWERCASE";
+let newValue = lower.toLowerCase();
+console.log(newValue);
+
+//Find the index of a character in a string.
+//Sol.4
+let index = "hello world";
+let strIndex = index.indexOf(" ");
+console.log(strIndex);
